685cca0bb823e41437eb6a0c5e3c64422d95d2cd
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-02-28 18:45:58 -0600

    ingreso de caracterisitcas del procesador y velocidad

---
+++ HOJA5.py
@@ -0,0 +1,7 @@
+import simpy 
+import random 
+import statistics
+#la cantidad de proceso deseado de manera ingresada por el usuario
+procesocant = int(input("Favor de ingresar la cantidad de procesos:\n "))
+#el intervalo ingresado por el usuario y no ingresado desde el sistema
+procesointer = int(input ("FAVOR DE INGRESAR EL INTERVALO QUE DESEA USAR:\n "))
